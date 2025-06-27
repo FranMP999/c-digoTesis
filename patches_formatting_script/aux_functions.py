cecc9911b08cedae36066de6f74c51a661a3a4cb
FranMP999 <f.maldonado@ug.uchile.cl>
2025-06-27 17:25:59 -0400

    código para procesamiento de parches a partir de productos Sentinel 2

---
+++ patches_formatting_script/aux_functions.py
@@ -0,0 +1,193 @@
+from pathlib import Path
+import pandas as pd 
+import numpy as np
+import xarray as xr
+import geopandas as gpd
+import rasterio
+from rasterio import features
+from shapely.geometry import box
+
+class RasterData:
+    def __init__(self, raster_reader, window):
+        self.bounds = box(*raster_reader.window_bounds(window))
+        self.transform = raster_reader.window_transform(window)
+        self.shape = (256, 256)
+        
+    def set_dates(self, dates):
+        self.dates = dates
+        return self
+
+def get_band_paths(product_path):
+    '''
+    Obtiene una lista con los paths de las bandas a partir del path de producto.
+    '''
+    return list(product_path.glob(
+        f"*_*_B*"
+        ))
+
+# Para acceder archivos de productos
+def path2band(path):
+    '''
+    Obtiene el código de banda de un producto Sentinel-2 a partir de su path.
+    '''
+    return str(path).split("/")[-1].split("_")[-2]
+
+def path2date(path):
+    '''
+    Entrega el datetime asociado a un producto Sentinel-2 a partir de su path.
+    '''
+    return pd.to_datetime(str(path).split("/")[-1].split("_")[1][:8])
+
+
+# Para trabajar patches
+def patch_coors(n, patch_size=256, array_size=1830):
+    '''
+    Retorna las coordenadas del punto superior izquierdo del patch n-ésimo.
+    Supone que todos los productos son de igual tamaño.
+    '''
+    lim = patch_size *(array_size // patch_size +1 )
+    x = (n * patch_size) % lim
+    if ((n+1) * patch_size) % lim == 0:# Condición último patch
+        x = array_size - patch_size
+
+    y= ((n * patch_size) // lim) * patch_size
+    if ((n * patch_size) // lim) * patch_size + patch_size > array_size:# Condición último patch
+        y = array_size - patch_size
+    return (x,y)
+
+
+
+def get_labels_in_tile(labels_path, tile_name, class_mapping, crs):
+    '''
+    Retorna un geopandas dataframe con las parcelas en el tile
+    a partir de un path, el nombre del tile, el mapeo hcat4-clases y un crs.
+    '''
+    return (
+        gpd.read_file(
+            labels_path,
+            where=f"name='{tile_name}'",
+        )
+        .to_crs(crs)
+        .assign(polygon=lambda df: df.geometry.map(lambda x: x.geoms[0]))
+        .assign(crop_class=lambda df: df.hcat4_code.map(class_mapping))
+    )
+
+
+
+def get_patch_rasterio(raster_reader, n, patch_size=256, get_data=False):
+    '''
+    Retorna el patch n-ésimo a partir de un Dataset Reader de rasterio
+    '''
+    array_size = raster_reader.shape[0]
+
+    x_patch, y_patch = patch_coors(n, patch_size, array_size)
+    window = rasterio.windows.Window.from_slices(
+            rows=slice(x_patch, x_patch + 256),
+            cols=slice(y_patch, y_patch + 256),
+        )
+    if get_data:
+        return raster_reader.read( 1, window=window), RasterData(raster_reader, window)
+    else:
+        return raster_reader.read( 1, window=window)
+
+def create_patch_tensor_rasterio(products_paths, patch_n):
+    '''
+    Retorna tupla (tensor, patch_bounds, lista_de_fechas) con tensor completo.
+    Recibe de una iterable con los paths de todos los productos del tile y el número de patch deseado.
+    '''
+    frames = []
+    #Se ordenan los path de productos por fecha
+    dates, sorted_paths = (lambda series: (series.index, series.values))(
+            pd.Series({ path2date(get_band_paths(path)[0]): path for path in products_paths })
+            .sort_index()
+            )
+
+    for product_path in sorted_paths:
+        band_arrays = []
+        i=0
+        for band_path in get_band_paths(product_path):
+            with rasterio.open(band_path) as src:
+                if i==0:
+                    band_raster, patch_data = get_patch_rasterio(src, patch_n, get_data=True)
+                    i+=1
+                else:
+                    band_raster = get_patch_rasterio(src, patch_n)
+            band_arrays.append(band_raster)
+        if len(band_arrays) == 12:
+            stack = np.stack(band_arrays, axis=0)  # (bands, N, N)
+            frames.append(stack)
+        else: print(f"PRODUCTO NO TIENE LAS 12 BANDAS: {product_path} ")
+    tensor_final = np.stack(frames, axis=0)
+    patch_data.set_dates(dates)
+    return tensor_final, patch_data  # (temporal, bands, N, N)
+
+def get_annotation_raster(patch_data: RasterData, labels_gdf):
+        tensor_bounds = patch_data.bounds
+        sel_parcels = labels_gdf[ labels_gdf.intersects(tensor_bounds) ]
+        shapes = list(zip(sel_parcels.polygon, sel_parcels.crop_class))
+        return rasterio.features.rasterize(
+            shapes,
+            out_shape = patch_data.shape,
+            fill = 0,
+            transform = patch_data.transform,
+            all_touched = False, # Esto lo tengo que revisar bien
+            dtype = None
+        )
+
+
+def get_id(tile_name, patch_n):
+    array_size = 10980
+    tiles = [
+        "31TBF",
+        "29TNF",
+        "30UXU",
+        "32TPP",
+        "32UMC",
+        "29UNU",
+        "33TVN",
+        "31UFU",
+        "35TMH",
+        "32VNH",
+    ]
+    tile_map = {tile: i for i, tile in enumerate(tiles)}
+    patchesxtile = (array_size//256 + 1)**2
+    total_n_patches = patchesxtile * len(tiles)
+    id = (tile_map[tile_name] * patchesxtile) + patch_n
+
+    assert patch_n < patchesxtile, "número de patch no válido"
+    assert tile_name in tiles, "tile no válido"
+    return id
+
+### LEGACY CODE ###
+
+def get_patch(xarray, n, patch_size=256):
+    '''
+    Retorna el patch n-ésimo.
+    Supone que todos los productos son de igual tamaño.
+    '''
+    array_size = xarray.x.size
+
+    patchesxtile = (array_size//patch_size + 1)**2
+    assert n < patchesxtile, "número de patch no válido"
+
+    x_patch, y_patch = patch_coors(n, patch_size, array_size)
+    return xarray.isel(
+            x=slice(x_patch, x_patch + 256),
+            y=slice(y_patch, y_patch + 256),
+        )
+def create_patch_tensor(products_paths, patch_n):
+        multiband_tensors = []
+        for product_path in products_paths:
+            multiband_tensors.append(
+                xr.concat([
+                    get_patch(
+                        xr.open_dataset(band_path, engine="rasterio", band_as_variable=True)
+                        #.drop_dims("band")
+                        .assign(band=path2band(band_path)).set_coords("band")
+                        .assign(time=path2date(band_path)).set_coords("time"),
+                        n = patch_n
+                    )
+                    for band_path in get_band_paths(product_path)
+                ], dim="band")
+            )
+        return xr.concat(multiband_tensors, dim="time")
---
+++ patches_formatting_script/format_patches.py
@@ -0,0 +1,123 @@
+import os
+from pathlib import Path
+import time
+import numpy as np
+import pandas as pd 
+
+import rasterio
+import geopandas as gpd
+
+
+from aux_functions import (
+    RasterData,
+    get_id,
+    get_labels_in_tile,
+    create_patch_tensor,
+    get_annotation_raster,
+    create_patch_tensor_rasterio,
+)
+
+sentinel_crs = 'EPSG:32629'
+in_path = Path("../data") 
+s2_path = in_path / "productos"
+labels_path = in_path / "gsa_2022_selectedtiles.gpkg"
+
+out_path = Path("data")
+metadata_path = out_path / "metadata.geojson"
+s2_out_path = out_path / "DATA_S2"
+annotations_out_path = out_path / "ANNOTATIONS"
+for path in [s2_out_path, annotations_out_path]:
+    if not path.exists(): os.makedirs(path)
+
+class_mapping_path = Path("class_mapping.csv")
+class_mapping = (
+    pd.read_csv(class_mapping_path, index_col=0)
+    .iloc[:,0]
+    .to_dict()
+)
+
+
+start = time.time()
+#Leer los tiles únicos en s2_path y loopear procedimiento para cada tile
+unique_tiles = set([
+    str(path).split("_")[-2][1:]
+    for path in s2_path.glob( f"*")
+    ])
+print(f"Tiles encontrados:\n\t", *list(unique_tiles), sep=" ")
+
+metadata_rows = []
+count=0
+for tile_name in unique_tiles:
+    print(f"Formateando tile {tile_name}...")
+
+    #Parcelas en tile
+    labels_gdf = get_labels_in_tile(
+        labels_path=labels_path,
+        tile_name=tile_name,
+        class_mapping=class_mapping,
+        crs=sentinel_crs,
+    )
+
+    #Crear tensor con bandas y tiempo, en el patch n-ésimo
+    array_size = 10980
+    patch_size = 256
+    final_n = (array_size//patch_size + 1)**2
+    for patch_n in range(0, final_n):
+        id = get_id(tile_name, patch_n)
+        print(f"\tFormateando patch {patch_n} (id={id})...")
+
+        time_series_tensor, raster_data = create_patch_tensor_rasterio(
+            products_paths=s2_path.glob(f"*{tile_name}*"),
+            patch_n=patch_n
+        )
+        # Crear el raster de etiquetado
+        annotation_raster = get_annotation_raster(
+                raster_data, 
+                labels_gdf
+        )
+
+        # Guardar resultados
+        np.save(
+            s2_out_path / f"S2_{"{:05}".format(id)}.npy",
+           time_series_tensor 
+        )
+        np.save(
+            annotations_out_path / f"ParcelIDs_{"{:05}".format(id)}.npy",
+            annotation_raster.astype(np.int16)
+        )
+
+        # Guardar metadata cada 5 patches
+        metadata_rows.append({
+            "id": id,
+            "tile_name": tile_name,
+            "patch_n": patch_n,
+            "parcel_cover": (annotation_raster > 0).sum() / annotation_raster.size,
+            "dates_S2": {
+                i: pd.to_datetime(date).strftime("%Y%m%d")
+                for i, date in enumerate(raster_data.dates)
+            },
+            "geometry": raster_data.bounds,
+        })
+        count = (count + 1)%5
+        if True :
+            #Almacenar metadata
+            metadata_gdf = (
+                gpd.GeoDataFrame(metadata_rows, geometry="geometry", crs=sentinel_crs)
+                .set_index("id")
+            )
+            if metadata_path.exists():
+                old_metadata_gdf = (
+                    gpd.read_file(metadata_path).set_crs(sentinel_crs, allow_override=True)
+                    .astype({"id": int})
+                    .set_index("id")
+                )
+                metadata_gdf = pd.concat([
+                    metadata_gdf,
+                    old_metadata_gdf,
+                    ]).reset_index().drop_duplicates(subset="id").set_index("id")
+            with open(metadata_path, "w") as text_file:
+                text_file.write(metadata_gdf.to_json())
+
+end = time.time()
+print("The time of execution of above program is :",
+      (end-start)/60 , "m")
